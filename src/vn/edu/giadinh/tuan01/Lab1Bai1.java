50459a91aa9ee555df5f78bd5cb6a34b0824acef
NguyenTruongSon0101 <sonn6272@gmail.com>
2021-09-25 13:25:55 +0700

    lần đẩy đầu tiên

---
+++ src/vn/edu/giadinh/tuan01/Lab1Bai1.java
@@ -0,0 +1,13 @@
+package vn.edu.giadinh.tuan01;
+import java.util.Scanner;
+public class Lab1Bai1 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tên sinh viên:");
+        String hoTen = scanner.nextLine();
+        System.out.println("Điểm trung bình:");
+        double diemTB = scanner.nextDouble();
+        System.out.printf("%s %.2f điểm",hoTen,diemTB);
+    }
+}
