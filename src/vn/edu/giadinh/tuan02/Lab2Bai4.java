b03dd744c9af691d180c61951457cbea65c8de78
NguyenTruongSon0101 <sonn6272@gmail.com>
2021-09-25 15:22:07 +0700

    Lap2Bai4 Chương trình menu

---
+++ src/vn/edu/giadinh/tuan02/Lab2Bai4.java
@@ -0,0 +1,44 @@
+package vn.edu.giadinh.tuan02;
+
+import java.util.Scanner;
+
+public class Lab2Bai4 {
+    public static void main(String[] args) {
+        System.out.println("==============Menu============");
+        System.out.println(">>1. Giải phương trình bậc I");
+        System.out.println(">>2. Giải phương trình bậc II");
+        System.out.println(">>3. Tính tiền điện");
+        System.out.println("==============Menu============");
+        int suLuaChon;
+        System.out.print("Lựa chọn chức năng:");
+        Scanner bienNhap;
+        bienNhap = new Scanner(System.in);
+        suLuaChon = bienNhap.nextInt();
+
+        switch(suLuaChon){
+            case 1: giaiPTBacI(); break;
+            case 2: giaiPTBacII(); break;
+            case 3: tinhTienDien(); break;
+            default: System.out.println("Chọn chức năng khác");
+        }
+    }
+
+    static void giaiPTBacI(){
+        float a = 0, b = 0;
+        
+        if(a == 0){
+            if(b == 0){
+                System.out.println("PT vô số nghiệm...");
+            }
+        }
+    }
+
+    
+    static void giaiPTBacII(){
+        System.out.println("Triển khai code giải phương trình bậc II ....");
+    }
+
+    static void tinhTienDien(){
+        System.out.println("Triển khai code tinh tien dien ....");
+    }
+}
